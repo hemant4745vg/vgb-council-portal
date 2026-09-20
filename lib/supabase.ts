1e6224dce888bc200a5992df673ece49aa33b892
hemant4745vg <hemant4745.vg@gmail.com>
2026-09-20 12:46:40 +0530

    Create supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,11 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://lllmgmfofwczpqbmigey.supabase.co";
+
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsbG1nbWZvZndjenBxYm1pZ2V5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk1NTIxNzYsImV4cCI6MjEwNTEyODE3Nn0.H_YfM8J3ZOy-B1lH7jgc4JtHu4rhUsigZ72qoI-b1ss";
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
